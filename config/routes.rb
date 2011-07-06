2ee11b5ccc2dcd6ce0e79cb9f4c07512c0095d93
Dani Donisa <dani.doni@gmail.com>
2011-07-06 12:37:20 +0200

    Added route support for archive access by day

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Refinery::Application.routes.draw do
   resources :events do
+    get 'archive/:year(/:month(/:day))' => 'events#archive', :as => 'archive', :on => :collection
     get 'archive/:year(/:month)' => 'events#archive', :as => 'archive', :on => :collection
     get 'category/:id' => 'event_categories#show', :as => 'category', :on => :collection
   end
